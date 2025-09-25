ae943939c871ba94d208a7da490ea71fb5908040
abmcompanysn-dot <abmcompanysn@gmail.com>
2025-09-25 17:04:08 +0000

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 /* ================================================================== */
 
 
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwnBuc9oXNkxyXdTsvhs7aed3yzCUW8WptSG0Tl58sK_EFCNE2GT91cQNYFfb2J5h_Opw/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw941I2P20rM3ru4sJjGoLaQi6DbOuhfVnfDqJMB8mLsht-JPWgh51mHGKfqXs2ZrTcOA/exec';
 // CORRECTION: Utilisation des noms de clés de session cohérents avec les autres fichiers.
 // --- Clés de session et autres constantes ---
 const ADMIN_SESSION_KEY = 'abm_admin_token';
@@ -17,3 +17,4 @@ const PAGE_URLS = {
     STUDENT: 'etudiant.html',
     RESPONSABLE: 'responsable.html'
 };
+
