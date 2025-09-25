d6fbe2bb0b6e662819adac4832a0c071b6f68dc7
abmcompanysn-dot <abmcompanysn@gmail.com>
2025-09-25 15:33:15 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+/* ================================================================== */
+/*               FICHIER DE CONFIGURATION DU FRONTEND                 */
+/* ================================================================== */
+
+
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwnBuc9oXNkxyXdTsvhs7aed3yzCUW8WptSG0Tl58sK_EFCNE2GT91cQNYFfb2J5h_Opw/exec';
+// CORRECTION: Utilisation des noms de clés de session cohérents avec les autres fichiers.
+// --- Clés de session et autres constantes ---
+const ADMIN_SESSION_KEY = 'abm_admin_token';
+const STUDENT_SESSION_KEY = 'abm_student_token';
+const RESPONSABLE_SESSION_KEY = 'abm_responsable_token';
+
+// --- URL des pages ---
+const PAGE_URLS = {
+    HOME: 'index.html',
+    ADMIN: 'admin.html',
+    STUDENT: 'etudiant.html',
+    RESPONSABLE: 'responsable.html'
+};
